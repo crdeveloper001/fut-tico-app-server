fcba866dd4a2ff824070b7b1347c249342f73a00
cgonzalezh23 <141349607+cgonzalezh23@users.noreply.github.com>
2023-08-09 18:54:02 -0600

    change name repo

--- src/main/java/com/example/futticoappserver/FutTicoAppServerApplication.java
+++ src/main/java/com/example/futticoappserver/FutTicoAppServerApplication.java
@@ -2,12 +2,10 @@ package com.example.futticoappserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = {"com.example.futticoappserver.repositories"})
-@ComponentScan(basePackages = {"com.example.*"})
+@EnableMongoRepositories(basePackages = {"com.example.futticoappserver.Repositories"})
 public class FutTicoAppServerApplication {
 
     public static void main(String[] args) {
